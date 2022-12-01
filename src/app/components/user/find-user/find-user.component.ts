985bb13bc8660791b641d98c14946a094e3f3748
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2022-12-01 14:07:06 +0100

    Miglioramenti visivi

--- src/app/components/user/find-user/find-user.component.ts
+++ src/app/components/user/find-user/find-user.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Users } from 'src/app/models/Users';
 import { UserService } from 'src/app/services/user.service';
-
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-find-user',
@@ -35,17 +35,28 @@ export class FindUserComponent implements OnInit{
   }
 
   findUser(multipleSearches: any) {
-    this.UserService.getUserById(this.id?.value).subscribe((u) => {
+    this.UserService.getUserById(this.id?.value).subscribe((fetched_user) => {
       console.log(this.switch?.value);
+      if (fetched_user != null) {
       if(this.switch?.value == true) {
-        this.users?.push(u);
+        this.users?.push(fetched_user);
       } else if(this.switch?.value == false || this.switch?.value == null){
         this.users = [];
         console.log(this.users)
-        this.user = u;
+        this.user = fetched_user;
       }
       this.showRecordFound = true;
       console.log(this.users?.length)
+    } else {
+      // Alert personalizzato che avvisa dell'avvenuta aggiunta dell'operatore
+      Swal.fire({
+        position: 'center',
+        icon: 'error',
+        title: 'Operatore non trovato',
+        showConfirmButton: false,
+        timer: 1500
+      });
+    }
     });
   }
 
